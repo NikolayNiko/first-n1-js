38838ae6b489c784d3b42be698fb2bfc1ad0e722
Myke <mioman72@gmail.com>
2023-09-02 18:39:20 +0300

    fc js

---
+++ js/main.js
@@ -0,0 +1,23 @@
+// alert('Hello, world and Mykola');
+
+// prompt('Enter name');
+
+// confirm('Are you agree?');
+
+// console.log('This is js');
+let a = 17;
+let b = 3;
+
+let summ = a + b;
+let minus = a - b;
+let multip = a * b;
+let division = a / b;
+let exponentiation = a**b;
+console.log(summ);
+console.log(minus);
+console.log(multip);
+console.log(division);
+console.log(exponentiation);
+console.log(Math.floor(a/b)); 
+
+// its my first solution to js from step
